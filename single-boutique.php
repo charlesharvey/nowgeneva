078d7dde1b0bf157b49a06d55641f6ae8864630d
Charles Harvey <harvey.charles@gmail.com>
2017-09-19 10:12:02 +0100

    get posts that mention shops

--- single-boutique.php
+++ single-boutique.php
@@ -14,30 +14,35 @@
         <div class="container">
 
 
-                <div class="row">
+            <div class="row">
 
 
-                    <div class="col-sm-3">
+                <div class="col-sm-3">
 
-                        <h1><span><?php the_title(); ?></span></h1>
-                        <?php  if ($boutique_cats) : ?>
+                    <h1><span><?php the_title(); ?></span></h1>
+                    <?php  if ($boutique_cats) : ?>
                         <p class="single-category"><?php  echo $boutique_cats[0]->name; ?></p>
-                        <?php endif; ?>
-                        <?php the_content(); // Dynamic Content ?>
+                    <?php endif; ?>
+                    <?php the_content(); // Dynamic Content ?>
 
-                    </div>
-                    <div class="col-sm-9">
-                            <?php $image = ( has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(),  'large') : ''; ?>
-                            <img src="<?php echo $image; ?>" alt="" />
-
-
-                            <div id="map_container"></div>
-                            <script type='text/javascript'>
-                                var location_address = '<?php echo get_field("address"); ?>' ;
-                            </script>
-
-                    </div>
                 </div>
+                <div class="col-sm-9">
+                    <?php $image = ( has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(),  'large') : ''; ?>
+                    <img src="<?php echo $image; ?>" alt="" />
+
+
+                    <div id="map_container"></div>
+                    <script type='text/javascript'>
+                    var location_address = '<?php echo get_field("address"); ?>' ;
+                    </script>
+
+                </div>
+            </div>
+
+
+
+            <?php get_template_part('partials/single', 'posts-from-shop'); ?>
+
 
 
 
