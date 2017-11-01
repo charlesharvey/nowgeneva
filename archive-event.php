cdd5ed70dedc5f9e0819cc4e3f160ac21007fd49
Charles Harvey <harvey.charles@gmail.com>
2017-11-01 15:28:24 +0000

    add archive page for events and portraits

---
+++ archive-event.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div class="container">
+
+			<h1><span><?php _e( 'Events & Ventes', 'webfactor' ); ?></span></h1>
+
+			<?php get_template_part('loop'); ?>
+
+			<?php get_template_part('pagination'); ?>
+
+</div>
+
+
+
+<?php get_footer(); ?>
--- nav.php
+++ nav.php
@@ -24,29 +24,37 @@ $supermenu .= '</div></div></div>';
 array_push($chilly_supermenus, $supermenu);
 
 
-
+$today = date("Y-m-d");
 $events = get_posts(array(
     'post_type' => 'event',
      'posts_per_page' =>  3,
-      'meta_key' => 'date',
+      'meta_key' => 'start_date',
       'orderby' => 'meta_value',
       'order' => 'ASC',
-      'meta_query' => array( // WordPress has all the results, now, return only the events after today's date
+      'meta_query' => array(
+          'relation' => 'OR',
           array(
-          'key' => 'date', // Check the start date field
-          'value' => date("Y-m-d"), // Set today's date (note the similar format)
-          'compare' => '>=', // Return the ones greater than today's date
-          'type' => 'DATE' // Let WordPress know we're working with date
-          )
+              'key' => 'start_date', // Check the start date field is
+              'value' => $today,
+              'compare' => '>=',
+              'type' => 'DATE'
+          ),
+          array(
+              'key' => 'end_date', // Check the start date field is
+              'value' => $today,
+              'compare' => '>=',
+              'type' => 'DATE'
+          ),
+
       )
  ));
 $supermenu = '<div id="supermenu_events" class="supermenu"><div class="container"><div class="row">';
 $supermenu .= '<div class="col-sm-3"><blockquote>Voluptate fidelissimae e officia e incurreret a minim mentitum. Labore eiusmod ita esse amet se sunt qui officia.</blockquote></div>';
 foreach ( $events as $post ) : setup_postdata( $post );
-$date = get_field('date');
+$start_date = get_field('start_date');
 $image = ( has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(),  'small') : '';
 $permalink = get_the_permalink();
-$supermenu .= '<div class="col-sm-3"><a  class="article_image" href="'. $permalink .'" style="background-image:url(' . $image . ');" title=""></a><h3><a href="'.get_the_permalink().'">'. get_the_title()  .'</a></h3><p class="the_date">Le '. $date .'</p></div>';
+$supermenu .= '<div class="col-sm-3"><a  class="article_image" href="'. $permalink .'" style="background-image:url(' . $image . ');" title=""></a><h3><a href="'.get_the_permalink().'">'. get_the_title()  .'</a></h3><p class="the_date">Le '. $start_date .'</p></div>';
 endforeach; wp_reset_postdata();
 $supermenu .= '</div></div></div>';
 array_push($chilly_supermenus, $supermenu);
@@ -94,8 +102,8 @@ array_push($chilly_supermenus, $supermenu);
 
              <?php chilly_nav('primary-navigation'); ?>
 
-            <li  class="supermenu_li" data-supermenu="supermenu_portraits"><a href="#">Portraits</a></li>
-            <li  class="supermenu_li" data-supermenu="supermenu_events"><a href="#">EVENTS &amp; VENTES</a></li>
+            <li  class="supermenu_li" data-supermenu="supermenu_portraits"><a href="<?php echo  $home_url; ?>/portrait/">Portraits</a></li>
+            <li  class="supermenu_li" data-supermenu="supermenu_events"><a href="<?php echo  $home_url; ?>/event/">EVENTS &amp; VENTES</a></li>
 
 
         </ul>
