0cdea06866d762f3ff29d970b76d9bee7bf66f93
Mélissa Rissel <rissel.melissa@gmail.com>
2017-11-01 09:55:10 +0000

    added infomaniak newsletter form to footer

--- footer.php
+++ footer.php
@@ -18,9 +18,23 @@
                             <div class="col-sm-4">
                                 <h4>Newsletter</h4>
                                 <p>Inscrivez-vous et accédez aux offres exclusives NowGeneva</p>
-                                <form action="#">
-                                    <input name="s" type="text" placeholder="enter your email">
-                                    <button >Envoyer</button>
+                                <form method="post" action="https://newsletter.infomaniak.com/external/submit" class="inf-form" target="_blank"><input type="email" name="email" style="display:none" /><input type="hidden" name="key" value="eyJpdiI6Imt0bUUremkzNGVXQTFtUysyajIzTTh4XC83ZW9cL1wvXC9LbUdyUHBtNG1zMjhnPSIsInZhbHVlIjoiaFVtTUM5TEdVbXFlcjhLQUkwZ1JQdklhZHV6amRWYXpqa3lvXC9CNEVRSWM9IiwibWFjIjoiMjcxODhmYTcxNTdkYWY3YTRiYTE4NTRmMjMyODFkOWRiZGJjZTVmM2VlYmUxYzg4ZjUwZmQ1ZjYzZDgxYzA3YiJ9">
+                                  <input type="hidden" name="webform_id" value="2826">
+                                  <div class="inf-main">
+                                    <div class="inf-success" style="display:none">
+                                      <h4>Merci pour votre inscription à la newsletter NOW Geneva!</h4>
+                                      <p>
+                                        <a href="#" class="inf-btn"><<</a>
+                                      </p>
+                                    </div>
+                                    <div class="inf-content">
+                                      <div class="inf-input inf-input-text">
+                                        <input type="text" name="inf[1]" data-inf-meta="1" data-inf-error="Veuillez renseigner votre adresse email" required="required" placeholder="Email" >
+                                      </div>
+                                      <div class="inf-submit"> <input type="submit" name="" value="Envoyer">
+                                      </div>
+                                    </div>
+                                  </div>
                                 </form>
                             </div>
                             <div class="col-sm-4">
