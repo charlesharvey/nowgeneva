3cefc28e1efa889b559140e87429c51745f45ef6
Charles Harvey <harvey.charles@gmail.com>
2018-02-09 08:26:24 +0000

    add grey out past events on slider if not enough to mkae up 3

--- partials/home-events.php
+++ partials/home-events.php
@@ -13,27 +13,59 @@ if ( $is_front_page  ) {
 $today = date( 'Y-m-d' );
 $evenements = new WP_Query(array(
     'post_type' => 'event',
-     'posts_per_page' =>  $posts_per_page,
-      'meta_key' => 'start_date',
-      'orderby' => 'meta_value',
-      'order' => 'ASC',
-      'meta_query' => array(
-          'relation' => 'OR',
-          array(
-              'key' => 'start_date', // Check the start date field is
-              'value' => $today,
-              'compare' => '>=',
-              'type' => 'DATE'
-          ),
-          array(
-              'key' => 'end_date', // Check the start date field is
-              'value' => $today,
-              'compare' => '>=',
-              'type' => 'DATE'
-          ),
+    'posts_per_page' =>  $posts_per_page,
+    'meta_key' => 'start_date',
+    'orderby' => 'meta_value',
+    'order' => 'ASC',
+    'meta_query' => array(
+        'relation' => 'OR',
+        array(
+            'key' => 'start_date', // Check the start date field is
+            'value' => $today,
+            'compare' => '>=',
+            'type' => 'DATE'
+        ),
+        array(
+            'key' => 'end_date', // Check the start date field is
+            'value' => $today,
+            'compare' => '>=',
+            'type' => 'DATE'
+        ),
 
-      )
+    )
 ));
+
+// build up slider with greyed out past events
+// if we dont have at least 3 future events
+$ev_count = $evenements->post_count;
+if (  $ev_count  < 3) {
+
+    $limit = 3 - $ev_count;
+    $past_evenements = new WP_Query(array(
+        'post_type' => 'event',
+        'posts_per_page' =>  $limit,
+        'meta_key' => 'start_date',
+        'orderby' => 'meta_value',
+        'order' => 'ASC',
+        'meta_query' => array(
+            'relation' => 'AND',
+            array(
+                'key' => 'start_date', // Check the start date field is
+                'value' => $today,
+                'compare' => '<',
+                'type' => 'DATE'
+            ),
+            array(
+                'key' => 'end_date', // Check the start date field is
+                'value' => $today,
+                'compare' => '<',
+                'type' => 'DATE'
+            ),
+
+        )
+    ));
+};
+
 ?>
 
 
@@ -41,13 +73,13 @@ $evenements = new WP_Query(array(
     <div class="container">
 
         <?php if ($is_front_page): ?>
-        <h2 ><span>Events &amp; Vente</span></h2>
+            <h2 ><span>Events &amp; Vente</span></h2>
         <?php endif; ?>
-                <p style="margin: -20px auto -20px; text-align: center; font-style: italic; font-size: 1.8em;"><a href="#newsletter">Inscrivez vous vite</a> pour recevoir en avant première les ventes privées, soldes ou offres privilégiées accordées aux abonnés(es) de Now Geneva.</p>
+        <p style="margin: -20px auto -20px; text-align: center; font-style: italic; font-size: 1.8em;"><a href="#newsletter">Inscrivez vous vite</a> pour recevoir en avant première les ventes privées, soldes ou offres privilégiées accordées aux abonnés(es) de Now Geneva.</p>
         <ul class="bxslider">
 
 
-            <?php  while($evenements->have_posts()) : $evenements->the_post();   ?>
+            <?php   while($evenements->have_posts()) : $evenements->the_post();  // future events  ?>
                 <?php $image = ( has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(),  'rectangle') : ''; ?>
                 <?php $url = get_the_permalink(); ?>
                 <?php $start_date = get_field('start_date'); ?>
@@ -55,45 +87,56 @@ $evenements = new WP_Query(array(
                 <?php $place = get_field('place'); ?>
                 <?php $boutiques = get_field('boutiques'); ?>
                 <li class="single_slide">
-
-
-
-                                    <div class="article_image_container">
-                                        <a href="<?php echo $url; ?>" class="">
-                                            <img src="<?php echo $image; ?>"/>
-                                        </a>
-                                    </div>
-
-
-
-                            <h4><a href="<?php echo $url; ?>">
-                              <?php if ($boutiques) echo $boutiques[0]->post_title; ?>
-                              <?php// if ($start_date) echo ' - ' . $start_date; ?>
-                              <?php// if ($end_date && $start_date && $start_date != $end_date) echo ' - ' .  $end_date; ?>
-                              <?php if ($place) echo ' - ' . $place; ?>
-                            </a></h4>
-
-                            <?php if ($start_date){?>
-                              <p class="time">
-                                <?php echo $start_date; ?>
-                                <?php if ($end_date && $start_date && $start_date != $end_date) echo ' - ' .  $end_date; ?>
-                              </p>
-                            <?php } ?>
-
-                            <p class="home_tl time_location">
-                              <?php echo get_the_title(); ?>
-                            </p>
-
-                            <p><?php //echo get_the_excerpt(); ?></p>
-                            <!-- <p><a style="margin-top:-20px" href="<?php echo $url; ?>" class="button">Lire plus</a></p> -->
-
-
+                    <div class="article_image_container">
+                        <a href="<?php echo $url; ?>" class="">
+                            <img src="<?php echo $image; ?>"/>
+                        </a>
+                    </div>
+                    <h4><a href="<?php echo $url; ?>">
+                        <?php if ($boutiques) echo $boutiques[0]->post_title; ?>
+                        <?php if ($place) echo ' - ' . $place; ?>
+                    </a></h4>
+                    <?php if ($start_date){?>
+                        <p class="time">
+                            <?php echo $start_date; ?>
+                            <?php if ($end_date && $start_date && $start_date != $end_date) echo ' - ' .  $end_date; ?>
+                        </p>
+                    <?php } ?>
+                    <p class="home_tl time_location">
+                        <?php echo get_the_title(); ?>
+                    </p>
+                    <p><?php //echo get_the_excerpt(); ?></p>
+                    <!-- <p><a style="margin-top:-20px" href="<?php echo $url; ?>" class="button">Lire plus</a></p> -->
                 </li>
-
-
-
-
             <?php  endwhile; ?>
+
+            <?php   while($past_evenements->have_posts()) : $past_evenements->the_post();  // past events  ?>
+                <?php $image = ( has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(),  'rectangle') : ''; ?>
+                <?php $url = get_the_permalink(); ?>
+                <?php $start_date = get_field('start_date'); ?>
+                <?php $end_date = get_field('end_date'); ?>
+                <?php $place = get_field('place'); ?>
+                <?php $boutiques = get_field('boutiques'); ?>
+                <li class="single_slide past_event">
+                    <div class="article_image_container">
+                            <img src="<?php echo $image; ?>"/>
+                    </div>
+                    <h4>
+                        <?php if ($boutiques) echo $boutiques[0]->post_title; ?>
+                        <?php if ($place) echo ' - ' . $place; ?>
+                    </h4>
+                    <?php if ($start_date){?>
+                        <p class="time">
+                            <?php echo $start_date; ?>
+                            <?php if ($end_date && $start_date && $start_date != $end_date) echo ' - ' .  $end_date; ?>
+                        </p>
+                    <?php } ?>
+                    <p class="home_tl time_location">
+                        <?php echo get_the_title(); ?>
+                    </p>
+                </li>
+            <?php  endwhile; ?>
+
 
 
         </ul>
