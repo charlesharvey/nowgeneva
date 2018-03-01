ccc2b257de3008765f87fa6c037e39dae163591e
Charles Harvey <harvey.charles@gmail.com>
2018-03-01 17:39:42 +0100

    add google analytics

--- footer.php
+++ footer.php
@@ -57,14 +57,15 @@
 		<?php $tdu = get_template_directory_uri(); ?>
         <script type='text/javascript' src='//maps.google.com/maps/api/js?key=AIzaSyC-BDJZU14ltCrYRPei33a4ZSQfJqRbxNY&#038;ver=4.8.1'></script>
 		<script type="text/javascript" src="<?php echo $tdu; ?>/js/min/scripts.bundle.js?v=<?php echo wf_version(); ?>"></script>
-		<script>
-		// (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		// (f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		// l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		// ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		// ga('send', 'pageview');
-		</script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-114588276-2"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-114588276-2');
+        </script>
 </div><!--  END OF REST OF SITE DIV -->
 	</body>
 </html>
