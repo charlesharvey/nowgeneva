02ea2c2fb64769d5155caff5b74793345832ad3a
Charles Harvey <harvey.charles@gmail.com>
2018-03-01 12:35:44 +0100

    show video on homepage based on tick box in options page

--- partials/home-video.php
+++ partials/home-video.php
@@ -1,13 +1,14 @@
     <?php $video = get_field('video_url', 'option'); ?>
     <?php $title = get_field('video_title', 'option'); ?>
     <?php $subtitle = get_field('video_subtitle', 'option'); ?>
+    <?php $show_vid = get_field('show_vid', 'option'); ?>
 
 
-
+<?php if ($show_vid) : ?>
 <div class="container">
   <h2><span><?php echo $title; ?></span></h2>
   <p class="category" style="text-align:center; margin: -30px 0 30px;"><?php echo $subtitle; ?></p>
-  <!-- <iframe width="560" height="350" style="width:100%" src="https://www.youtube.com/embed/u70uSeycrVY?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> -->
   <div class="home_vid"><?php echo $video; ?></div>
 
 </div>
+<?php endif; ?>
